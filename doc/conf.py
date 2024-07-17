2c9f29944662e9847d36e26ed460b4bbc409c2db
Prayag Yadav <prayagyadav206@gmail.com>
2024-07-17 12:47:23 +0530

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -27,3 +27,15 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #html_theme = 'sphinx_rtd_theme'
 html_theme = 'sphinx_nefertiti'
 html_static_path = ['_static']
+
+# -- Theme options ------------------------------------------------------------
+# https://sphinx-nefertiti.readthedocs.io/en/latest/quick-start.html#customize-the-theme
+html_theme_options = {
+  "repository_name": "prayagyadav/coffea-fcc-analyses",
+  "repository_url": "https://github.com/prayagyadav/coffea-fcc-analyses",
+  "footer_links": ",".join([
+    "Documentation|https://prayagyadav.github.io/coffea-fcc-analyses",
+    "Repository|https://github.com/prayagyadav/coffea-fcc-analyses",
+    "Issues|https://github.com/prayagyadav/coffea-fcc-analyses/issues",
+  ])
+}
